661179249e9b42729fb5eff935878157f19046dd
PanosPavlou <polykarposepi@hotmail.gr>
2017-11-30 23:45:53 +0200

    panos

--- Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/services/eforia/EgrafhStoSistimaService.java
+++ Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/services/eforia/EgrafhStoSistimaService.java
@@ -1,5 +1,8 @@
 package aueb.mcsis.omada7.services.eforia;
 
 public class EgrafhStoSistimaService {
-
+	 // to service auto elexgxei an uparxei allh etairia me to idio afm
+	 // an to afm einai egkiro kai exoun elex8ei ta stoixeia tote stelnei mnm epitixous eggrafhs
+	 //kataxwrei thn eggrafh sth vash logariasmoi !!!
+	 // save // o panos ton pernei//
 }
