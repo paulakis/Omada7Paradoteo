b11d95d4f72b46c8e1b3f9334fa0a3939b471354
PanosPavlou <polykarposepi@hotmail.gr>
2017-12-03 01:32:18 +0200

    mporei kai telos sta services

--- Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/services/eforia/TropopoihshEggrafhsService.java
+++ Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/services/eforia/TropopoihshEggrafhsService.java
@@ -1,6 +1,12 @@
 package aueb.mcsis.omada7.services.eforia;
 
+import java.util.Date;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import aueb.mcsis.omada7.domain.eforia.Dhlwsh;
+import aueb.mcsis.omada7.domain.eforia.LogariasmosEtairias;
 
 public class TropopoihshEggrafhsService {
 	//BOOLEAN AN TO AFM EINAI TO IDIO
@@ -9,12 +15,31 @@ public class TropopoihshEggrafhsService {
 
 	private EntityManager em;
 
+
 	public TropopoihshEggrafhsService(EntityManager em) {
 		super();
 		this.em = em;
 	}
 	
+	// elegxetai automata apo ton eforo.erwthsh(?)
+	public void TropopoihshLogariasmou(String afm,String epwnimia,String email,int thlefwno,Date hmeromhniasistashs){
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		LogariasmosEtairias eter = em.find(LogariasmosEtairias.class, afm);
+		if(eter!=null){
+			eter.setEpwnimia(epwnimia);
+			eter.setEmail(email);
+			eter.setThlefwno(thlefwno);
+			eter.setHmeromhniasistashs(hmeromhniasistashs);
+			eter.setExeiElefthei(false);
+			eter.setNeedCheck(true);
+			em.persist(eter);
+			tx.commit();
+			em.close();
+		}
+	}
 	
 
+	
 
 }
