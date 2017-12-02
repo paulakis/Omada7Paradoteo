fc3dbebded626ab84fbdb8b95aa6f6d8e2b8d566
PanosPavlou <polykarposepi@hotmail.gr>
2017-12-02 15:51:31 +0200

    kai allh allagh

--- Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/services/eforia/IpovolhDhlwshsService.java
+++ Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/services/eforia/IpovolhDhlwshsService.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 
+import com.mgiandia.library.domain.Borrower;
+
 import aueb.mcsis.omada7.domain.eforia.Dhlwsh;
 import aueb.mcsis.omada7.domain.eforia.LogariasmosEtairias;
 import aueb.mcsis.omada7.domain.eforia.Parastatiko;
@@ -52,10 +54,27 @@ public class IpovolhDhlwshsService {
 		
 		return d ;
 	}
+	//vriskei mia dhlwsh me to id
+	public Dhlwsh VresDhlwshById(int id ){
+		Dhlwsh d;
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		try {
+			d = em.find(Dhlwsh.class, id);
+			tx.commit();
+		} catch (NoResultException ex) {
+			d = null;
+			tx.rollback();
+		}
+		return d;
+	}
+	
 	
 	
 	//vazoume kai ta parastatika afou exoume kanei nea dhlwsh
 	public Parastatiko ValeNeoParastatiko(){
+		//prwta mallon kalei th sinarthsh gia na vrei thn dhlwsh
+		//metaa vazei sth dhlwsh to kataallhlo parastatiko.
 		return new Parastatiko();
 	}
 	
