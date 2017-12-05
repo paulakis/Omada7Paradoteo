fac6ed13dbd35bde019b56c8fc1b4f498e2dfa6a
fourakiss <32496977+fourakiss@users.noreply.github.com>
2017-12-05 21:03:21 +0200

    //mitsuko

--- Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/domain/eforia/ElegxosApaths.java
+++ Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/domain/eforia/ElegxosApaths.java
@@ -12,7 +12,8 @@ public class ElegxosApaths extends AithmaElegxou {
 	boolean apath;
 	private  Dhlwsh d;
 	
-	
+//sinoliko poso exodown-esodwn diafora <<0 episterfei boolean apath kai ekxorei sth vadh apati
+	//apo mian dhlwsh athroizei ola ta parastatika kai vgazei to sum
 	
 	public void Ipopsifiaapath(Dhlwsh d){
 		
