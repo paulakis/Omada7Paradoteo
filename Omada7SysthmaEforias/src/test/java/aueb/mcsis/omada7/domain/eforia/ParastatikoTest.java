864524cbb85e7c1ee1293617c6ddc95367106bd1
fourakiss <32496977+fourakiss@users.noreply.github.com>
2017-12-09 15:21:08 +0200

    fou

--- Omada7SysthmaEforias/src/test/java/aueb/mcsis/omada7/domain/eforia/ParastatikoTest.java
+++ Omada7SysthmaEforias/src/test/java/aueb/mcsis/omada7/domain/eforia/ParastatikoTest.java
@@ -1,22 +1,39 @@
 package aueb.mcsis.omada7.domain.eforia;
-import org.junit.Test;
-
 import org.junit.*;
+import java.util.Date;
+
 
 public class ParastatikoTest {
 
+	
 	@Test
 	public void pareToPosoTest(){
 		Parastatiko p = new Parastatiko();
 		p.setEidossinallaghs(true);
 		p.setPoso(1);
-		Assert.assertTrue(p.isEidossinallaghs() && p.getPoso() > 0);
+		p.setId(1);
+		p.setAfmsimvalwmenoou("123456789");
+		p.setArithmosparastatikou(1);
+		 Date a= new Date();
+p.setHmeromhniaparastatikou(a);
+		Assert.assertTrue(p.isEidossinallaghs()&& p.getPoso() > 0 &&  p.PareToPoso(p) > 0);
 	}
 	
 	@Test
 	public void pareToPosoArnitikoTest(){
 		Parastatiko p = new Parastatiko();
-		Assert.assertFalse(p.isEidossinallaghs() && p.getPoso() < 0);
+		p.setEidossinallaghs(false);
+		p.setPoso(1);
+		
+		Assert.assertFalse(p.PareToPoso(p)<0 && (p.getPoso() < 0) && (p.isEidossinallaghs()));
 	}
 	
+	@Test
+	public void pareToPosoNullTest(){
+		Parastatiko p = new Parastatiko();
+		Assert.assertTrue(p.getPoso()==0 && p.PareToPoso(p)==0);
 }
+}
+
+    
+	
