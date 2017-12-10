16c328cdbde3aa0bdca3427988639a3ced176a70
spyridonf <spyros30013athens@hotmail.com>
2017-12-10 22:09:45 +0200

    +2,1

--- Omada7SysthmaEforias/src/test/java/aueb/mcsis/omada7/domain/eforia/AithmaElegxouTest.java
+++ Omada7SysthmaEforias/src/test/java/aueb/mcsis/omada7/domain/eforia/AithmaElegxouTest.java
@@ -8,6 +8,7 @@ public class AithmaElegxouTest {
 
 	@Test
 	public void aploTest(){
+		AithmaElegxou aitElnull = new AithmaElegxou();
 		AithmaElegxou aitEl = new AithmaElegxou(true, true, true);
 		aitEl.setId(33);
 		aitEl.setDinatothtatropo(false);
@@ -16,6 +17,7 @@ public class AithmaElegxouTest {
 		Assert.assertFalse(aitEl.isDinatothtatropo());
 		Assert.assertFalse(aitEl.isEnhmerwsheforou());
 		Assert.assertFalse(aitEl.isOloklhrwshelegxwn());
+		Assert.assertEquals(33, aitEl.getId());
 		
 	}
 }
