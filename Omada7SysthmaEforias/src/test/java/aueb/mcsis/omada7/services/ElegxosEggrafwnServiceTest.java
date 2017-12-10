7b220791ad255c3e18ec324fccd1f3dc0ef4a83d
paulakis <polykarposepi@hotmail.gr>
2017-12-10 18:58:07 +0200

    neo service

---
+++ Omada7SysthmaEforias/src/test/java/aueb/mcsis/omada7/services/ElegxosEggrafwnServiceTest.java
@@ -0,0 +1,28 @@
+package aueb.mcsis.omada7.services;
+
+
+import org.junit.Test;
+
+import aueb.mcsis.omada7.services.eforia.ElegxosEggrafwnService;
+
+public class ElegxosEggrafwnServiceTest extends GenikoServiceTest {
+	
+	ElegxosEggrafwnService e;
+
+	public ElegxosEggrafwnServiceTest() {
+		super();
+	}
+	
+	
+	@Test 
+	public void TestFeretouslogariasmouspoudenexounelegxthei(){
+		
+	}
+	
+
+	@Test
+	public void TestAllakseKatastashLogariasmou(){
+		
+	}
+	
+}
