755a77f8b7873dd1448dc0abfd1433edc2805858
spyridonf <spyros30013athens@hotmail.com>
2017-12-10 05:16:37 +0200

    oh boy

--- Omada7SysthmaEforias/src/test/java/aueb/mcsis/omada7/services/ElegxosGiaApathServiceTester.java
+++ Omada7SysthmaEforias/src/test/java/aueb/mcsis/omada7/services/ElegxosGiaApathServiceTester.java
@@ -1,48 +1,37 @@
 package aueb.mcsis.omada7.services;
 
-import javax.persistence.EntityManager;
-
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import aueb.mcsis.omada7.domain.eforia.ElegxosApaths;
 import aueb.mcsis.omada7.persistence.eforia.*;
 import aueb.mcsis.omada7.services.eforia.ElegxosGiaApathService;
+import aueb.mcsis.omada7.domain.eforia.Dhlwsh;
 
-
-public class ElegxosGiaApathServiceTester {
+public class ElegxosGiaApathServiceTester extends GenikoServiceTest{
 	
-	Initializer dataHelper;
-	private EntityManager em;
 
-		
 	public ElegxosGiaApathServiceTester() {
 		super();
 	}
 
 
-	@Before
-	public final void setUp() {
-		dataHelper = new Initializer();
-		dataHelper.prepareData();
-		em = JPAUtil.getCurrentEntityManager();
-		
-	}
+
 	// prepei na doume ama exoume kapoia apath mesa ston pinaka 
 	 
 	
 	@Test
 	public void testFindElegxosApathsById()
 	{
-		
+	
 	}
+	
 	
 	@Test
 	public void testfindDhlwshById(){
 	
 	}
+	
+	
 	
 	@Test
 	public void testGirnaeiToAfmThsApaths(){
@@ -54,10 +43,7 @@ public class ElegxosGiaApathServiceTester {
 	
 	
 	
-	@After
-	public final void tearDown() {
-		em.close();
-	}
+
 
 	
 }
