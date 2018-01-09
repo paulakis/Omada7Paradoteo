ed4f7b23fe6ae5598c854e3318b8b478ed07afd0
PanosPavlou <polykarposepi@hotmail.gr>
2018-01-09 21:14:18 +0200

    asdasdasd

--- Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/resource/eforia/EggrafhResource.java
+++ Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/resource/eforia/EggrafhResource.java
@@ -1,10 +1,25 @@
 package aueb.mcsis.omada7.resource.eforia;
 
+import java.util.List;
+import aueb.mcsis.omada7.domain.eforia.LogariasmosEtairias;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 @Path("Logariasmos")
 public class EggrafhResource extends AbstractResource {
-
+	
+	@Context
+	UriInfo uriInfo;
+	
+	@GET
+	@Produces("application/xml")
+	public List<LogariasmosEtairias> DwseOlousTousLogariasmous(){
+		return null;
+	}
 	
 	
 }
--- Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/resource/eforia/ElegxoiResource.java
+++ Omada7SysthmaEforias/src/main/java/aueb/mcsis/omada7/resource/eforia/ElegxoiResource.java
@@ -2,4 +2,5 @@ package aueb.mcsis.omada7.resource.eforia;
  
 public class ElegxoiResource extends  AbstractResource {
 
+	
 }
